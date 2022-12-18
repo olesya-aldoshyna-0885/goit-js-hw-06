f8bb13b6e05bc23b9ea898723adc88dd32a45b3e
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2022-12-18 21:43:28 +0200

    черновик task-01

---
+++ js/task-01.js
@@ -0,0 +1,30 @@
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+document.querySelectorAll(#categories);
+
+// ul.classList.add(elementCategories);
+
+// document.querySelectorAll();
+// const array = [];
+// array.push();
+
+
+
+// calculatorCategorirs(Category) {
+// const liElement = 
+// };
+
+// document.querySelectorAll()
---
+++ js/task-02.js
@@ -0,0 +1,23 @@
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const newIngredients =
+  function createNewElement() {
+    for (ingredient of ingredients) {
+      ingredient.document.createElement(`<li></li>`);
+    };
+    console.log(ingredient);
+  };
